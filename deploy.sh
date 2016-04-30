8cf3a8f1a2c63fa8bfbb26b9837d40b5e9a24e3e
Vojtech Bartos <barvoj@seznam.cz>
2016-04-30 11:31:40 +0200

    Change port to php container

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,5 @@ mkdir $BUILD_NAME
 mv * $BUILD_NAME
 tar -czf build.tgz $BUILD_NAME
 
-scp -p 11138 -i id_dsa -o 'StrictHostKeyChecking no' build.tgz app@alpha-node-2.rosti.cz:/srv/
-ssh app@alpha-node-2.rosti.cz -p -i id_dsa 11138 -o 'StrictHostKeyChecking no' "tar xfz  /srv/build.tgz && /srv/${BUILD_NAME}/install.sh ${BUILD_NAME}"
+scp -p 11160 -i id_dsa -o 'StrictHostKeyChecking no' build.tgz app@alpha-node-2.rosti.cz:/srv/
+ssh app@alpha-node-2.rosti.cz -p -i id_dsa 11160 -o 'StrictHostKeyChecking no' "tar xfz  /srv/build.tgz && /srv/${BUILD_NAME}/install.sh ${BUILD_NAME}"
